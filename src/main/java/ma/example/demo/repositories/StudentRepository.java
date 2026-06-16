7de034204712561d9b5875addb464a4a452b165a
BOUHADRI IKRAM <ikrambouhadri202@gmail.com>
2026-06-16 15:43:18 +0100

    ajout fichiers manquants

--- src/main/java/ma/example/demo/repositories/StudentRepository.java
+++ src/main/java/ma/example/demo/repositories/StudentRepository.java
@@ -1,4 +1,17 @@
 package ma.example.demo.repositories;
 
-public interface StudentRepository {
+import ma.example.demo.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    Student findById(int id);
+
+    @Query("SELECT YEAR(s.dateNaissance), COUNT(s) FROM Student s GROUP BY YEAR(s.dateNaissance)")
+    Collection<Object[]> findNbrStudentByYear();
 }
